73ed9d592ebaf0339f860d28aa3192ad89358c2f
Olga Filchukova <makeeva.o.y@gmail.com>
2023-08-31 17:27:46 +0300

    cors added

--- app.js
+++ app.js
@@ -11,6 +11,40 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 
+const allowedCors = [
+  'https://movies-app.nomoredomainsicu.ru',
+  'http://movies-app.nomoredomainsicu.ru',
+  // 'https://api.movies-app.nomoredomainsicu.ru',
+  // 'http://api.movies-app.nomoredomainsicu.ru',
+  'http://localhost:3000',
+  'http://localhost:3001',
+];
+
+app.use((req, res, next) => {
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  // проверяем, что источник запроса есть среди разрешённых
+  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+  // Значение для заголовка Access - Control - Allow - Methods по умолчанию
+  // (разрешены все типы запросов)
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (allowedCors.includes(origin)) {
+    // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    // разрешаем кросс-доменные запросы с этими заголовками
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    // завершаем обработку запроса и возвращаем результат клиенту
+    return res.status(200).send();
+  }
+
+  return next();
+});
+
 const { PORT = 3000 } = process.env;
 
 mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb');
