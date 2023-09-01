13ce9f1d5f5578846b2415f82659774c70489466
Olga Filchukova <makeeva.o.y@gmail.com>
2023-09-01 16:04:03 +0300

    mongo bugs fixed_1

--- app.js
+++ app.js
@@ -48,9 +48,7 @@ const { PORT = 3000 } = process.env;
 
 // mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb');
 // mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
-mongoose.connect(NODE_ENV === 'production' ? DATA_BASE : 'mongodb://localhost:27017/bitfilmsdb', {
-  useNewUrlParser: true,
-});
+mongoose.connect(NODE_ENV === 'production' ? DATA_BASE : 'mongodb://127.0.0.1:27017/bitfilmsdb');
 
 app.listen(PORT, () => { console.log(`App listening on port ${PORT}`); });
 
